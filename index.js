cc2a02052115b689cff654967c984b7c8c6965d4
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-06 15:48:05 +0300

    Type of

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
- let username;
+//  let username;
  
- document.getElementById("myButton").onclick = () => {
-    username = document.getElementById("myText").value;
-    console.log(username); 
+//  document.getElementById("myButton").onclick = () => {
+//     username = document.getElementById("myText").value;
+//     console.log(username); 
 
-     document.getElementById("myLabel").innerHTML = "Hello "+username;
+//      document.getElementById("myLabel").innerHTML = "Hello "+username;
     
 
- } 
+let x;
+let y;
+let z;
+
+x= Number("22");
+y = String(34);
+z = Boolean("")
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
