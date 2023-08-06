9982ce4b40ff13e4fed2d087ec6b5e79f500b577
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-06 16:08:11 +0300

    math

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // Math
 let x = 3.14;
+let y = 2;
+let z = 9;
 
 x = Math.round(x); //correct round of
 x = Math.floor(x); //round down ans will be 3
 x = Math.ceil(x); //round up ans will be 4
 x = Math.pow(x,2); //power ans will be 3
 x = Math.sqrt(x);
-x = Math.abs(x)  //distance value from zero
+x = Math.abs(x);  //distance value from zero
 
-console.log(x);
+maximum = Math.max(x, y, z);
+minimum = Math.min(x, y, z);
+
+console.log(minimum);
+console.log(maximum);
