5b9a5002f19ebe6e895e582fea75cd66369e4edd
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-06 22:45:45 +0300

    radio buttons

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 
-// if statement   
+// radio buttons 
  document.getElementById("myButton").onclick = function(){
-    const checkBox = document.getElementById("myCheckBox")
-   if (checkBox.checked=== true) {
-    alert("you are subscribed")
     
-   } else {
-    alert("You are not subscribed")
+    const visa =  document.getElementById("visa");
+    const mastercard = document.getElementById("mastercard");
+    const paypal = document.getElementById("paypal");
+
+   if (visa.checked=== true) {
+    alert("you are paying with visa");
     
-   }
+   } else if(mastercard.checked){
+    alert("You are paying with mastercard");
+    } else if(paypal.checked){
+        alert("you are paying with paypal");
+    } else{
+        alert("select payment method");
+    }
+     
  }
  
 
