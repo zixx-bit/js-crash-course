bc93979539568f8b3849911169e8fecb39b5005e
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-07 15:22:14 +0300

    functions

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 // nested loop = a loop inside another loop 
 
-let rows = window.prompt("enter number of rows");
-let columns = window.prompt("enter number of columns");
+// function = define code and use it many TimeRanges. 
+//             To perfom some code, call the function name
+
+startProgram();
 
 
-for (let i = 1; i <= rows; i++) {
-   
-    for (let j = 1; j <= columns; j++) {
-        document.getElementById("myRectangle").innerHTML +=j;
-    }
-    document.getElementById("myRectangle").innerHTML += "<br>"; 
+function startProgram(){
+    let name = "Edd Michira";
+    let age = "28";
+
+    happyBirthday(name, age);
+
+}
+
+
+
+function happyBirthday(name, age){
     
+console.log("Happy birthday to you!");
+console.log("Happy birthday to you!");
+console.log("Happy birthday dear __", name);
+console.log("You are", age," __ yaers old");
+
 }
