fb2eb5a9e6ec4fb14e3123c0f60ef8c91a13825b
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-07 10:39:57 +0300

    while loop

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
+// while loop
+                // repaet some code while some condition is true 
+                // potentially infinte  
+let userName = "";
 
-// switch statement
-// switch = statement that examines a value for a match against many clauses.
-// more efficient than many "else if" statements
-let grade;
-
-    grade = window.prompt("Enter your score");
-   
-    grade = Number(grade);
-
-    console.log(grade);
-
-    switch (true) {
-        case grade >= 90:
-            console.log("you did great.You gogt an A");
-            break;
-
-            case grade >= 70:
-                console.log("you got a B");
-                break;
-            
-        case grade >= 50:
-            console.log("You got a C");     
-            break;
-            
-        case grade >= 40:
-            console.log("You got a D");
-            break;
-    
-        default:
-            console.log("you did not do an exam")
-            break;
+    while(userName == "" || userName == null){
+        userName = window.prompt("enter your name ");
     }
+    
 
- 
-
+    console.log("Hello", userName);
