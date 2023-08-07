71626882a8c4a54c2a118b1449cf6afbb644fd47
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-07 10:49:02 +0300

    do while

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// while loop
-                // repaet some code while some condition is true 
-                // potentially infinte  
-let userName = "";
+// do while loop = do something ,
+                // then check the condition, reoeat if the condition is true
+                 
+let userName;
 
-    while(userName == "" || userName == null){
+    do {
         userName = window.prompt("enter your name ");
-    }
-    
+       
+    } while (userName == "" || userName == null);
 
+   
     console.log("Hello", userName);
