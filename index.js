018612354091e1389fbd6373917c522f4879b829
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-07 13:08:59 +0300

    nested loop to create a rectangle

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // nested loop = a loop inside another loop 
 
+let rows = window.prompt("enter number of rows");
+let columns = window.prompt("enter number of columns");
 
-for (let i = 1; i <=2; i++) {
+
+for (let i = 1; i <= rows; i++) {
    
-    for (let j = 1; j <=3; j++) {
-            console.log(j) ;       
+    for (let j = 1; j <= columns; j++) {
+        document.getElementById("myRectangle").innerHTML +=j;
     }
+    document.getElementById("myRectangle").innerHTML += "<br>"; 
     
 }
