15a25df594066228b7b8dd34dd2c56770be9caaa
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-13 23:07:31 +0300

    temperature conversion

--- index.js
+++ index.js
@@ -16,3 +16,41 @@ let text =`hello ${userName}, you have ${items} in your cart. Your total is ${to
 console.log(text);
 document.getElementById("myLabel").innerHTML = text;
 
+
+
+   document.getElementById("tempButton").onclick = function () {
+   
+    let temp;
+
+    if (document.getElementById("cButton").checked === true) 
+    {
+         temp = document.getElementById("textbox").value;   
+        temp = Number(temp);  
+        temp = toCelcius()
+        document.getElementById("fButton").innerHTML = temp + "C";
+
+    } else if(document.getElementById("fButton").checked === true) 
+    {
+         temp = document.getElementById("textbox").value;   
+        temp = Number(temp);  
+        temp = toFarenHeight()
+        document.getElementById("fButton").innerHTML = temp +"F";
+     
+    } else{
+        document.getElementById("fButton").innerHTML = "Select a unit"
+
+    }   
+    
+}
+
+   function toCelcius(){
+     return (temp - 32)* (5/9);
+   }
+
+   function toFarenHeight(){
+       return temp   * 9/5 + 32 ;
+   }
+
+    
+    
+
